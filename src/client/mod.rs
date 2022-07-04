84beaa7fef5d3ee7e08a8ff91173d48c1163cc54
juliankrieger <julian.krieger@me.com>
2022-07-04 17:14:18 +0200

    readjust compiler error messages

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -10,13 +10,13 @@ pub use reqwest::blocking::*;
 
 #[cfg(all(feature = "async", feature = "blocking"))]
 compile_error!(
-    "`client-reqwest` and `client-ureq` features cannot both be enabled at \
-    the same time, if you want to use `client-ureq` you need to set \
+    "`async` and `blockingf` features cannot both be enabled at \
+    the same time, if you want to use blocking` you need to set \
     `default-features = false`"
 );
 
 #[cfg(not(any(feature = "async", feature = "blocking")))]
 compile_error!(
     "You have to enable at least one of the available clients with the \
-    `client-reqwest` or `client-ureq` features."
+    `async` or `blocking` features."
 );
