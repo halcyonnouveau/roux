f9f131f75d7cf91f28a89fefaa56f1b59d1bf3b3
Brett Mayson <brett@mayson.io>
2026-09-05 11:31:33 +0000

    fix login

--- src/util/url.rs
+++ src/util/url.rs
@@ -1,9 +1,9 @@
 /// Builds a url for read only Reddit access.
 pub fn build_url(dest: &str) -> String {
-    format!("https://www.reddit.com/{}/.json", dest)
+    format!("https://www.reddit.com/{}", dest)
 }
 
 /// Builds a url for OAuth Reddit access.
 pub fn build_oauth(dest: &str) -> String {
-    format!("https://oauth.reddit.com/{}/.json", dest)
+    format!("https://oauth.reddit.com/{}", dest)
 }
